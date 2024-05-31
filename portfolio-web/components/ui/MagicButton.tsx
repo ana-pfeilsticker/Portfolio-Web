c9d2b9f823f9ee829f0b90462676c0f2c8b64236
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-05-31 10:13:18 -0300

    fix:button

--- portfolio-web/components/ui/MagicButton.tsx
+++ portfolio-web/components/ui/MagicButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const MagicButton = ({
   title,
   icon,
@@ -15,14 +16,13 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
              bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
